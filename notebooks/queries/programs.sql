a78232ac22bea4657231100a9145adf5890f3034
Miguel <garciarmiguel@uniovi.es>
2024-04-27 19:01:28 +0200

    First version completed

--- notebooks/queries/programs.sql
+++ notebooks/queries/programs.sql
@@ -1,6 +1,5 @@
 SELECT
     program_id as program__program_id,
-    name as program__name,
     has_sub_dirs_with_code as program__has_sub_dirs_with_code,
     has_packages as program__has_packages,
     number_of_modules as program__number_of_modules,
@@ -11,7 +10,6 @@ SELECT
     enum_defs_pct as program__enum_defs_pct,
     has_code_root_package as program__has_code_root_package,
     average_defs_per_module as program__average_defs_per_module,
-    user_id as program__user_id,
     expertise_level as program__expertise_level
 FROM programs;
 
