3b3d894c59ea24070013003bee3359185e88f4a8
Abel Busto Dopazo <UO284262@uniovi.es>
2024-05-13 16:12:19 +0200

    Methoddefs finished

--- dataset/test/test_file/test.py
+++ dataset/test/test_file/test.py
@@ -1,11 +1,5 @@
-"AHAHAHAHHAHA"
-import ast
-ast_1 = ast.parse(
-"""
-a = 2
-b = 5
-print(a+b)
-"""
-)
-
-print(ast_1)
+class A:
+    @wraps
+    @cache
+    def a(self):
+        pass
---
+++ notebooks/queries/methoddefs.sql
@@ -0,0 +1,10 @@
+SELECT
+    is_class_method as methoddef__is_class_method,
+    is_static_method as methoddef__is_static_method,
+    is_constructor_method as methoddef__is_constructor_method,
+    is_abstract_method as methoddef__is_abstract_method,
+    is_property as methoddef__is_property,
+    is_wrapper as methoddef__is_wrapper,
+    is_cached as methoddef__is_cached,
+    expertise_level as methoddef__expertise_level
+FROM methoddefs;
