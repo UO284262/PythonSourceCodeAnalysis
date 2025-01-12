4acc681a1db5cfdaa612d98fbec3bc515b35dd56
Abel Busto <UO284262@uniovi.es>
2025-01-12 18:52:55 +0100

    From EXPERT term to PROFESSIONAL

--- notebooks/notebooks_utils.py
+++ notebooks/notebooks_utils.py
@@ -186,7 +186,7 @@ def load_data(table: str) -> pd.DataFrame:
     print(datetime.now(), 'Data successfully load!!')
     return full_table
 
-
+"""
 def print_histogram(data: pd.DataFrame, column: str, expertise_column: str, bins: int = 30, include_all: bool = True, include_beginners: bool = True, include_experts: bool = True, min_value: float = None, max_value: float = None):
     plt.figure(figsize=(12, 6))
 
@@ -203,13 +203,58 @@ def print_histogram(data: pd.DataFrame, column: str, expertise_column: str, bins
     if include_all:
         plt.hist(data[column], bins=num_bins, alpha=0.5, label='All', color='blue')
     if include_experts:
-        plt.hist(data[data[f'{expertise_column}_EXPERT'] == 1][column], bins=num_bins, alpha=0.5, label='Experts', color='green')
+        plt.hist(data[data[f'{expertise_column}_PROFESSIONAL'] == 1][column], bins=num_bins, alpha=0.5, label='Experts', color='green')
     if include_beginners:
-        plt.hist(data[data[f'{expertise_column}_EXPERT'] == 0][column], bins=num_bins, alpha=0.5, label='Beginners', color='red')
+        plt.hist(data[data[f'{expertise_column}_PROFESSIONAL'] == 0][column], bins=num_bins, alpha=0.5, label='Beginners', color='red')
 
     plt.xlabel(column)
     plt.ylabel('Frequency')
     plt.title(f'{column} by expertise level histogram')
+    plt.legend()
+    plt.show()
+"""
+
+def print_histogram(data: pd.DataFrame, column: str, expertise_column: str, bins: int = 30, include_all: bool = True, include_beginners: bool = True, include_experts: bool = True, min_value: float = None, max_value: float = None):
+    plt.figure(figsize=(12, 6))
+
+    # Filtrar según valores mínimos y máximos
+    if min_value is not None:
+        data = data[data[column] >= min_value]
+    if max_value is not None:
+        data = data[data[column] <= max_value]
+
+    data_min = data[column].min()
+    data_max = data[column].max()
+
+    # Crear bins
+    num_bins = np.linspace(data_min, data_max, bins + 1)
+
+    # Calcular el total de instancias para los porcentajes
+    total_count = len(data)
+
+    if include_all:
+        counts, _ = np.histogram(data[column], bins=num_bins)
+        percentages = counts / total_count * 100
+        plt.hist(num_bins[:-1], num_bins, weights=percentages, alpha=0.5, label='All', color='blue')
+
+    if include_experts:
+        expert_data = data[data[f'{expertise_column}_PROFESSIONAL'] == 1]
+        expert_count = len(expert_data)
+        counts, _ = np.histogram(expert_data[column], bins=num_bins)
+        percentages = counts / expert_count * 100  # Porcentaje en relación a los expertos
+        plt.hist(num_bins[:-1], num_bins, weights=percentages, alpha=0.5, label='Professional', color='green')
+
+    if include_beginners:
+        beginner_data = data[data[f'{expertise_column}_PROFESSIONAL'] == 0]
+        beginner_count = len(beginner_data)
+        counts, _ = np.histogram(beginner_data[column], bins=num_bins)
+        percentages = counts / beginner_count * 100  # Porcentaje en relación a los principiantes
+        plt.hist(num_bins[:-1], num_bins, weights=percentages, alpha=0.5, label='Beginners', color='red')
+
+    # Etiquetas y leyenda
+    plt.xlabel(column)
+    plt.ylabel('Percentage')
+    plt.title(f'{column} by expertise level histogram (Percentage)')
     plt.legend()
     plt.show()
 
@@ -229,8 +274,8 @@ def print_categorical_histogram(data: pd.DataFrame, column: str, expertise_colum
         beginners_data['Group'] = 'Beginners'
         combined_data = pd.concat([combined_data, beginners_data])
     if include_experts:
-        experts_data = data[data[expertise_column] == 'EXPERT'].copy()
-        experts_data['Group'] = 'Experts'
+        experts_data = data[data[expertise_column] == 'PROFESSIONAL'].copy()
+        experts_data['Group'] = 'Professionals'
         combined_data = pd.concat([combined_data, experts_data])
     if vertical:
         sns.catplot(
